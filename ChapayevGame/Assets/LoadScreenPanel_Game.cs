1669cf15d3682d16b92e33e566a8bbc5940b8618
Denis Khasanov <denishasanov828@gmail.com>
2025-03-15 19:57:05 +0500

    0.1.6.9

---
+++ ChapayevGame/Assets/LoadScreenPanel_Game.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScreenPanel_Game : MovePanel
+{
+    [SerializeField] private AnimationFrame animationFrame;
+
+    public override void ActivatePanel()
+    {
+        base.ActivatePanel();
+
+        animationFrame.Activate(1);
+    }
+
+    public override void DeactivatePanel()
+    {
+        base.DeactivatePanel();
+
+        animationFrame.Deactivate();
+    }
+}
